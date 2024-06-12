25265a743891abcb5634473b66437980838c862f
quinterodiego <d86webs@gmail.com>
2024-06-12 06:38:17 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home />),
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
