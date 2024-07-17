6e41c038e9979fa82f5e7ebb592ac4b3ece0e536
quinterodiego <d86webs@gmail.com>
2024-07-17 17:00:17 -0300

    asd

--- src/features/product/components/ProductListLanding.js
+++ src/features/product/components/ProductListLanding.js
@@ -1,37 +1,24 @@
+import img01 from '../../../assets/hero01.png'
+import img02 from '../../../assets/hero02.png'
+import img03 from '../../../assets/hero03.png'
+import img04 from '../../../assets/hero04.png'
+import img05 from '../../../assets/hero05.png'
+import img06 from '../../../assets/hero06.png'
+import img07 from '../../../assets/hero07.png'
+
 const products = [
-  {
-    id: 1,
-    name: 'Earthen Bottle',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-  },
-  {
-    id: 2,
-    name: 'Nomad Tumbler',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-  },
-  {
-    id: 3,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$89',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-  },
-  {
-    id: 4,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-  },
-  // More products...
+  { obj: img01, name: 'producto 01', price: 3000 },
+  { obj: img02, name: 'producto 01', price: 3000 },
+  { obj: img03, name: 'producto 01', price: 3000 },
+  { obj: img04, name: 'producto 01', price: 3000 },
+  { obj: img05, name: 'producto 01', price: 3000 },
+  { obj: img06, name: 'producto 01', price: 3000 },
+  { obj: img07, name: 'producto 01', price: 3000 },
+  // { obj: img01, name: 'producto 01' },
+  // { obj: img01, name: 'producto 01' },
+  // { obj: img01, name: 'producto 01' },
+  // { obj: img01, name: 'producto 01' },
+  // { obj: img01, name: 'producto 01' },
 ]
 
 export default function ProductListLanding() {
@@ -41,24 +28,24 @@ export default function ProductListLanding() {
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Nuestro Productos</h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
+          {products.map((product, i) => (
+            <div key={i} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
-                  alt={product.imageAlt}
-                  src={product.imageSrc}
+                  alt={product.obj}
+                  src={product.obj}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+                    {/* <a href={product.href}> */}
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                    </a>
+                    {/* </a> */}
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
                 </div>
                 <p className="text-sm font-medium text-gray-900">{product.price}</p>
               </div>
