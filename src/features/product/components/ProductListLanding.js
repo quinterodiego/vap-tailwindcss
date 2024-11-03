cc984ce9e4145e804b583f1d921a4b01845f26fa
quinterodiego <d86webs@gmail.com>
2024-11-02 22:08:57 -0300

    swipperjs

--- src/features/product/components/ProductListLanding.js
+++ src/features/product/components/ProductListLanding.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import img01 from '../../../assets/productos/alcancia.jpg'
 import img02 from '../../../assets/productos/llavero.jpg'
-import img03 from '../../../assets/productos/valija.jpg'
+import valija01 from '../../../assets/productos/valija.jpg'
+import valija02 from '../../../assets/productos/valija02.jpg'
+import bolsitaGolosinera01 from '../../../assets/productos/bolsitas golosineras/01.jpg'
+import bolsitaGolosinera02 from '../../../assets/productos/bolsitas golosineras/02.jpg'
+import bolsitaGolosinera03 from '../../../assets/productos/bolsitas golosineras/03.jpg'
+import cajaExplosivaSorpresa01 from '../../../assets/productos/caja explosiva sorpresa/01.jpg'
+import cajaExplosivaSorpresa02 from '../../../assets/productos/caja explosiva sorpresa/02.jpg'
+import yoyo01 from '../../../assets/productos/yo-yo/01.jpg'
+import yoyo02 from '../../../assets/productos/yo-yo/02.jpg'
+import difusorPlastico01 from '../../../assets/productos/difusores plasticos/01.jpg'
+import difusorPlastico02 from '../../../assets/productos/difusores plasticos/02.jpg'
+import tarjetaBautimoComunion01 from '../../../assets/productos/tarjetas bautismo comunion/01.jpg'
+import tarjetaBautimoComunion02 from '../../../assets/productos/tarjetas bautismo comunion/02.mp4'
+import individual01 from '../../../assets/productos/individuales/01.jpg'
 
 const products = [
   { 
-    obj: img01, 
+    obj: [img01], 
     name: 'Alcancias plásticas', 
     src: '/src/assets/productos/alcancia.jpg', 
     price: 1000,
@@ -15,7 +32,7 @@ const products = [
     stock: 'Disponible'
   },
   { 
-    obj: img02, 
+    obj: [img02], 
     name: 'Llavero de acrilico con cadena',
     src: '/src/assets/productos/llavero.jpg', 
     price: 800,
@@ -25,13 +42,73 @@ const products = [
     stock: 'Disponible' 
   },
   { 
-    obj: img03, 
+    obj: [valija01, valija02], 
     name: 'Valijas medianas', 
     src: '/src/assets/productos/valija.jpg', 
     price: 1200,
     description: 'Sticker personalizado sobre un lado',
     dimensions: '14cm de ancho x 10cm de alto (sin manija) x 4cm de profundidad - Altura total con manija 13cm',
     colors: 'Pastel - Clásicos - Transparente',
+    stock: 'Disponible' 
+  },
+  { 
+    obj: [bolsitaGolosinera01, bolsitaGolosinera02, bolsitaGolosinera03], 
+    name: 'Bolsitas golosineras de papel', 
+    src: '/src/assets/productos/bolsitas golosineras/01.jpg', 
+    price: 400,
+    description: 'Con sticker. Mínimo 10 unidades.',
+    dimensions: '-',
+    colors: 'Varios',
+    stock: 'Disponible' 
+  },
+  { 
+    obj: [cajaExplosivaSorpresa01, cajaExplosivaSorpresa02], 
+    name: 'Caja explosiva sorpresa', 
+    src: '/src/assets/productos/bolsitas golosineras/01.jpg', 
+    price: 4000,
+    description: 'Frases y fotos. Base reforzada ideal para soportar tazas.',
+    dimensions: '12cm x 12cm',
+    colors: '-',
+    stock: 'Disponible' 
+  },
+  { 
+    obj: [yoyo01, yoyo02], 
+    name: 'Yo-yo', 
+    src: '/src/assets/productos/bolsitas golosineras/01.jpg', 
+    price: 1150,
+    description: 'Se venden de a 10 unidades. Vinilos de ambos lados. Se entregan embolsados con cierrabolsa personalizado',
+    dimensions: '-',
+    colors: 'Fuertes y pastel',
+    stock: 'Disponible' 
+  },
+  { 
+    obj: [difusorPlastico01, difusorPlastico02], 
+    name: 'Difusores plásticos 125cc.', 
+    src: '/src/assets/productos/bolsitas golosineras/01.jpg', 
+    price: 1750,
+    description: 'Difurores personalizados con exquisitos aromas. Tapas a elección. Incluyen varillas. Se entregan embolsados con cinta a tono. Fragancias: limón, lavanda, chicle, coco/vainilla, sandia/pepino, uva.',
+    dimensions: '-',
+    colors: '-',
+    stock: 'Disponible' 
+  },
+  { 
+    obj: [tarjetaBautimoComunion01, tarjetaBautimoComunion02], 
+    name: 'Tarjetas Bautismo/Comunión', 
+    src: '/src/assets/productos/bolsitas golosineras/01.jpg', 
+    price: 6000,
+    description: 'Papel texturado brilloso. X 20 unidades',
+    dimensions: '10cm x 6cm',
+    colors: '-',
+    stock: 'Disponible' 
+  },
+  { 
+    obj: [individual01], 
+    name: 'Individuales', 
+    src: '/src/assets/productos/bolsitas golosineras/01.jpg', 
+    price: '3000 - $4000',
+    description: 'Papel brilloso 115grs. 10 unidades (fondo blanco) $3000 - 10 unidades (fondo color) $4000',
+    dimensions: '20cm x 30cm',
+    colors: '-',
     stock: 'Disponible' 
   },
   // { obj: img01, name: 'producto 01' },
@@ -61,8 +138,8 @@ export default function ProductListLanding() {
               <div className="group relative">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   <img
-                    alt={product.obj}
-                    src={product.obj}
+                    alt={product.obj[0]}
+                    src={product.obj[0]}
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                   />
                 </div>
@@ -90,8 +167,22 @@ export default function ProductListLanding() {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
-                <img className='img-fluid' src={productoSeleccionado.obj} alt="" />
-                
+                <Swiper
+                  pagination={{
+                    dynamicBullets: true,
+                  }}
+                  modules={[Pagination]}
+                  className="mySwiper"
+                >
+                  {
+                    productoSeleccionado.obj.map(producto => (
+                      <SwiperSlide>
+                        <img className='img-fluid' src={producto} alt="" />
+                      </SwiperSlide>
+                    ))
+                  }
+                  
+                </Swiper>
                 <p className='pt-2'><b>Precio:</b> ${productoSeleccionado.price}</p>
                 <p className='pt-2'><b>Detalles:</b> {productoSeleccionado.description}</p>
                 <p className='pt-2'><b>Dimensiones:</b> {productoSeleccionado.dimensions}</p>
